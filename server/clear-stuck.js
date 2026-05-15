723f17af744638668fc48e13ae6be716b5f06b1b
mohanvel026 <mohanvel026@gmail.com>
2026-05-15 11:11:57 +0530

    Fix MONGO_URI in clear-stuck.js

--- server/clear-stuck.js
+++ server/clear-stuck.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGO_URI)
   .then(async () => {
     const Deployment = require('./src/models/Deployment.model');
     const Project = require('./src/models/Project.model');
